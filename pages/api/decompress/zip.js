9420e30508943fac169d74f7ca7635170c3f5aad
slender1808 <jadson.g-matos@outlook.com>
2021-11-03 02:08:27 -0300

    decompress get

--- pages/api/decompress/zip.js
+++ pages/api/decompress/zip.js
@@ -1,34 +1,14 @@
-import fs from "fs";
-import path from "path";
 import unzipper from "unzipper";
 
-export const config = {
-  api: {
-    bodyParser: false,
-    //externalResolver: true,
-  },
-};
-
 export default async function handle(req, res) {
-  if (req.query.blob) {
-    console.log(req.query.blob);
-    //const blob = Buffer.from(req.query.blob , 'base64');
-
-    await fs.writeFileSync("/tmp/tmp.zip", req.query.blob,'base64');
-
-    const zip = fs
-      .createReadStream("/tmp/tmp.zip")
-      .pipe(unzipper.Parse({ forceStream: true }));
-
-    for await (const entry of zip) {
-      console.log(entry);
+  if (req.method == "GET") {
+    if (req.query.blob) {
+      console.log(req.query.blob);
+      const blob = Buffer.from(req.query.blob, "base64");
+      const zip = await unzipper.Open.buffer(blob);
+      res.send(await zip.files[0].buffer());
     }
-    //const directory = await unzipper.Open.buffer(unescape(req.query.blob));
-    //console.log("directory", directory);
-
-    res.json({});
-    //console.log(req.query.blob);
-    //const zip = decodeURIComponent(req.query.blob)
-    //console.log(zip);
+  } else if (req.method == "POST") {
+    res.status(500).json({});
   }
 }
